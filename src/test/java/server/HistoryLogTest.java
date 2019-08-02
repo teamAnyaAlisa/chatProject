cb1ec347e7ad6cd7d3426037a82978b3a71289fa
Evgeny Kolesnik <java.course.2018@gmail.com>
2019-08-02 17:30:01 +0400

    Added HistoryLogTest

--- src/test/java/server/HistoryLogTest.java
+++ src/test/java/server/HistoryLogTest.java
@@ -1,47 +1,26 @@
 package server;
 
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import org.junit.Test;
+
+
+import java.io.IOException;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 
 /**
  * Created by Java_1 on 01.08.2019.
  */
-//@Ignore
-//public class HistoryLogTest {
-//    @Before
-//    public void setUp() throws Exception {
-//
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//
-//    }
-//
-//    @Test
-//    public void shouldLogCorrectly() throws Exception {
-//        HistoryLog logger = new HistoryLog();
-//        logger.log("123");
-//        logger.log("Привет!");
-//        BufferedReader in = new BufferedReader(
-//                new InputStreamReader(
-//                        new BufferedInputStream(
-//                                new FileInputStream(
-//                                        logger.getPath()))));
-//        assertEquals("123", in.readLine());
-//        assertEquals("Привет!", in.readLine());
-//    }
-//
-//    @Test
-//    public void shouldGetHistoryCorrectly() throws Exception {
-//        HistoryLog logger = new HistoryLog();
-//        logger.log("123 ");
-//        logger.log("hello!");
-//        assertEquals("123 hello!", logger.getHistory());
-//    }
-//
-//}
+public class HistoryLogTest {
+
+    @Test
+    public void shouldContainLoggedStringsInHistory()throws IOException {
+            HistoryLog logger = new HistoryLog();
+
+            logger.log("string 1");
+
+            String message = logger.getHistory();
+            assertThat(message.contains("string 1"));
+    }
+}
